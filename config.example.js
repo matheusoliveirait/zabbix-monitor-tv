3218d8855642de6fc2d7369ef40d2110f98d228f
Matheus Oliveira <146141745+matheusoliveirait@users.noreply.github.com>
2026-07-13 16:22:21 -0300

    Adiciona exemplo de configuracao local

---
+++ config.example.js
@@ -0,0 +1,11 @@
+window.HPRO_CONFIG = {
+  ZABBIX_API_URL: "http://192.168.0.7/zabbix/api_jsonrpc.php",
+  ZABBIX_TOKEN: "",
+  REFRESH_SECONDS: 10,
+  API_LIMIT: 500,
+  PAGE_INTERVAL_SECONDS: 15,
+  SORT_MODE: "recent",
+  FETCH_MODE: "incidents",
+  MONITORED_GROUP_IDS: [],
+  MONITORED_HOST_IDS: []
+};
